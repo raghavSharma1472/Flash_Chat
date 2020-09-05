662eef94e2b828209ea55b587cbce36ba660364c
raghavSharma1472 <grandraghav@outlook.com>
2020-09-05 21:58:42 +0530

    Code Inserted

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+final _firestore = Firestore.instance;
 
+FirebaseUser loggedInUser;
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
   @override
@@ -8,6 +13,23 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  String messageText;
+  int counter = 0;
+  void getCurrentUser() async {
+    try{
+      loggedInUser = await _auth.currentUser();
+      print(loggedInUser.email+' signedIn');
+    }catch(e){
+      print(e);
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +39,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+
               }),
         ],
         title: Text('⚡️Chat'),
@@ -28,6 +50,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -35,15 +58,21 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      messageTextController.clear();
+                      _firestore.collection('messages').add({
+                        'sender': loggedInUser.email,
+                        'message': messageText,
+                        'number': {counter++}.toString()
+                      });
                     },
                     child: Text(
                       'Send',
@@ -55,6 +84,93 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').orderBy('number').snapshots(),
+      builder: (context, snapshot) {
+        if(!snapshot.hasData){
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data.documents.reversed;
+        List<RoundedBubble> messagesWidgets = [];
+        for(var message in messages){
+          final messageText = message.data['message'];
+          final messageSender = message.data['sender'];
+          final currentUser = loggedInUser.email;
+          final messageWidget = RoundedBubble(
+              text: messageText,
+              sender: messageSender,
+              isMe: messageSender == currentUser,
+          );
+          messagesWidgets.add(messageWidget);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messagesWidgets,
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+class RoundedBubble extends StatelessWidget {
+  RoundedBubble({@required this.text,@required this.sender,@required this.isMe});
+  final String sender;
+  final String text;
+  final bool isMe;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+              sender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54
+            ),
+          ),
+          Material(
+            elevation: 5.0,
+            borderRadius: isMe ? BorderRadius.only(
+              topLeft: Radius.circular(25.0),
+              bottomLeft: Radius.circular(25.0),
+              bottomRight: Radius.circular(25.0),
+            ) : BorderRadius.only(
+              topRight: Radius.circular(25.0),
+              bottomRight: Radius.circular(25.0),
+              bottomLeft: Radius.circular(25.0),
+            ),
+            color: isMe ? Colors.lightBlueAccent : Colors.white ,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: isMe ? Colors.white : Colors.black54,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -18,7 +18,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   void initState() {
     super.initState();
     controller = AnimationController(
-      duration: Duration(seconds: 1),
+      duration: Duration(seconds: 3),
       vsync: this,
     );
     animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
